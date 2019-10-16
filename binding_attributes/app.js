e28f61f92e8841dd68d356b499fe0b442aabaf52
Andriy Gushuley <andriy.gushuley@playtech.com>
2019-10-16 16:58:30 +0300

    Initial import

---
+++ binding_attributes/app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/socks-green.jpg",
+        altText: "A pair of socks",
+        otherProductsUrl: "https://suvasocks.com/"
+    }
+})
