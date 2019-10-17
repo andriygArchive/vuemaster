78771d3849a2208485aab1ba6cb81d8ef2df3833
Andriy Gushuley <andriy.gushuley@playtech.com>
2019-10-17 13:59:00 +0300

    Course passing

--- 07_computed_properties/app.js
+++ 07_computed_properties/app.js
@@ -2,25 +2,28 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
         details: ["80% cotton", "20% polyester", "Gender-neytral"],
         variants: [
             {
                 id: 2234,
                 color: "green",
-                image: "../assets/socks-green.jpg"
+                image: "../assets/socks-green.jpg",
+                quantity: 100,
+                onSale: false,
             }, {
                 id: 2235,
                 color: "blue",
-                image: "../assets/socks-blue.jpg"
+                image: "../assets/socks-blue.jpg",
+                quantity: 0,
+                onSale: true,
             }
         ],
+        selectedVariant: 0,
         sizes: ["30-33", "34-37", "38-41", "42-45"],
-        image: "../assets/socks-green.jpg",
         altText: "A pair of socks",
         otherProductsUrl: "https://suvasocks.com/",
-        inStock: true,
-        onSale: true,
         cart: 0,
     },
     methods: {
@@ -34,8 +37,22 @@ var app = new Vue({
             }
         },
 
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(selectedVariant) {
+            this.selectedVariant = selectedVariant
         },
-    }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].onSale
+        }
+    },
 })
